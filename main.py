4de5459ef484ee2448b7fd0bb8811a47730f269f
manav <manavkhadka2004@gmail.com>
2025-02-28 06:45:34 +0530

    🚩Implement chat functionality with web search and response generation

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 
+from pydantic_models.chat_body import ChatBody
+from services.search_service import SearchService
+
 
 app = FastAPI()
+
+search_service = SearchService()
 
 @app.get("/read_root")
 def read_root():
     return {"Hello": "World"}
+
+# chat
+@app.post("/chat")
+def send_chat(body: ChatBody):
+    # Search the web and find appropriate sources
+    # sort the sources
+    # generate the response using LLM
+    search_results = search_service.web_search(body.query)
+    print(body.query)
+    return {"chat_response": {"query": body.query, "search_results": search_results}}
---
+++ services/search_service.py
@@ -0,0 +1,25 @@
+from config import Settings
+from tavily import TavilyClient
+import trafilatura
+
+settings = Settings()
+tavily_client = TavilyClient(api_key=settings.TAVILY_API_KEY)
+class SearchService:
+    def web_search(self, query: str):
+        results = []
+        response = tavily_client.search(query, max_results=10)
+        search_results = response.get("results", [])
+        for result in search_results:
+            url = result.get("url")
+            if url:
+                downloaded = trafilatura.fetch_url(url)
+                content = trafilatura.extract(downloaded)
+                results.append(
+                    {
+                        "title": result.get("title"),
+                        "url": url,
+                        "content": content
+                    }
+                )
+            
+        return results  
